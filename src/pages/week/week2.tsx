59b0a5ce77a95f6049df9c5fbb0cb591a277200c
Atharv Gangwar <152419242+AtharvGangwar48@users.noreply.github.com>
2025-02-25 23:52:01 +0530

    Add files via upload

---
+++ src/pages/week/week2.tsx
@@ -0,0 +1,9 @@
+function week2() {
+    return ( 
+    <>
+    <h1>Week2</h1>
+    </> 
+    );
+}
+
+export default week2;
---
+++ src/pages/week/week3.tsx
@@ -0,0 +1,9 @@
+function week3() {
+    return ( 
+    <>
+    <h1>Week1</h1>
+    </> 
+    );
+}
+
+export default week3;
